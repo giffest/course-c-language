86a3778c93f8836f84051c05914646c76a944083
Dmitry <s.dmitry.s@gmail.com>
2019-03-15 12:01:03 +0300

    Учимся выводить приветствие

---
+++ Epizod1/main.c
@@ -0,0 +1,14 @@
+/*
+ * maincod.c
+ *
+ *  Created on: Mar 11, 2019
+ *      Author: stingray
+ */
+
+#include <stdio.h>
+
+int main (int argc, const char* argv[]) {
+	printf("Hello, World! \n");
+
+	return 0;
+}
